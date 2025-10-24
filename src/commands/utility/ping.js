30a7a189eff9b626b5f1baf4e5374c3166ec8f50
YaroslavRomanenko <yaroslavromanenko456@gmail.com>
2025-10-24 13:38:13 +0300

    feat: add events and EventHandler, also provide connection to RestAPI

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
+    cooldown: 5,
     data: new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
     async execute(interaction) {
         await interaction.reply('Pong!');
--- src/commands/utility/user.js
+++ src/commands/utility/user.js
@@ -1,11 +1,29 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { execute } = require('./ping');
 
 module.exports = {
+    cooldown: 5,
     data: new SlashCommandBuilder().setName('user').setDescription('Provides information about the user'),
     async execute(interaction) {
+        const username = await userInfo();
         await interaction.reply(
-            `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`,
+            `Username: ${username}`,
         );
     },
+}
+
+const userId = '1094957058706644992';
+
+const url = `http://localhost:8080/api/username/${userId}`;
+const testUrl = 'http://localhost:8080/api/test'
+
+const getRequest = new Request(url, {
+   method: "GET" 
+});
+
+const userInfo = async () => {
+    const response = await fetch(getRequest);
+    const myJson = await response.json();
+
+    console.log(myJson.username);
+    return myJson.username;
 }
